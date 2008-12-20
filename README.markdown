4fd94f288078ff099a30256241b817e9b2b0b857
Georg Alexander Boe <georg.boe@gmail.com>
2008-12-20 21:53:58 +0100

    initial commit

---
+++ README.markdown
@@ -0,0 +1,25 @@
+## git_updater
+`git_updater.sh` to automate the process of updating git. It will try to find out from git.or.cz what the latest version is and download git and the manpages. The tarballs are checked using [GPG][] and the script will stop if the verification fails.
+
+### Requirements
+* The usual tools for building git (gcc, make, zlib, etc.)
+* [Gnu Privacy Guard (GPG)][GPG]
+* curl
+
+### Usage
+
+Depending on where you are installing git, you will need to run the script as root.
+The following command will download and install the latest version of git:
+
+	% sudo ./git_updater.sh
+
+You can also install a specific version by using an argument:
+
+	% sudo ./git_updater.sh 1.6.0.6
+
+### Notes
+* The script is set to use the z-shell located at `/bin/zsh`, but will also work in `bash`.
+* The script is intended for use on OS X Leopard, but have been used successfully in Ubuntu.
+
+
+[GPG]: http://gnupg.org/
---
+++ git_updater.sh
@@ -0,0 +1,71 @@
+#!/bin/zsh
+
+check_errs()
+{
+  # Function. Parameter 1 is the return code
+  # Para. 2 is text to display on failure.
+  if [ "${1}" -ne "0" ]; then
+    echo "ERROR # ${1} : ${2}"
+    exit ${1}
+  fi
+}
+
+# Check that we're root
+if [ `id -u` != 0 ]; then
+	echo "This script must be run as root"
+	exit 1
+fi
+
+# Check for an argument
+if [ -n "$1" ]; then
+	GIT_VERSION=$1
+else
+	# matches: <a href="http://www.kernel.org/pub/software/scm/git/docs/RelNotes-1.5.5.1.txt">
+    GIT_VERSION=`curl git.or.cz 2> /dev/null | sed -n 's/.*-\(.*\).txt.*/\1/p'`
+    echo "Current version expected to be $GIT_VERSION"
+    echo "Is this is incorrect, press CTRL-C NOW!"
+    sleep 5
+fi
+
+# Download the tarballs
+curl -O http://kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.gz \
+     -O http://kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.gz.sign \
+     -O http://kernel.org/pub/software/scm/git/git-manpages-$GIT_VERSION.tar.bz2 \
+     -O http://kernel.org/pub/software/scm/git/git-manpages-$GIT_VERSION.tar.bz2.sign
+
+# ===============
+# = Install Git =
+# ===============
+
+# Verify the git tarballs
+gpg --verify git-$GIT_VERSION.tar.gz.sign git-$GIT_VERSION.tar.gz
+check_errs $? "Git doesn't verify!"
+
+# Extract and install
+tar -xzvf git-$GIT_VERSION.tar.gz
+cd git-$GIT_VERSION
+./configure && make install
+cd ..
+
+# ============
+# = Manpages =
+# ============
+
+# Verify  and install manpages
+gpg --verify git-manpages-$GIT_VERSION.tar.bz2.sign git-manpages-$GIT_VERSION.tar.bz2
+check_errs $? "Manpages doesn't verify!"
+tar xjv -C /usr/local/share/man -f git-manpages-$GIT_VERSION.tar.bz2
+
+# ============
+# = Clean up =
+# ============
+
+rm -rf git-$GIT_VERSION.tar.gz git-$GIT_VERSION.tar.gz.sign \
+	   git-manpages-$GIT_VERSION.tar.bz2 git-manpages-$GIT_VERSION.tar.bz2.sign \
+       git-$GIT_VERSION
+
+
+echo "================================="
+echo "   GIT successfully updated!   "
+echo "      \c"; git --version
+echo "================================="
